e4c040f180670b453822df913c02a477935c21c7
tobezero01 <ducnhuad@gmail.com>
2025-01-24 09:07:05 +0700

    Test add Project in CLI

--- windowformc#/NhuDinhDuc-211202522/NhuDinhDuc-211202522/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NhuDinhDuc_211202522.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
